9607bbe7a9b195a7da386eab2e1e765351ef3ee7
amusictheorist <a_millaresthomson@outlook.com>
2024-12-11 15:13:49 -0500

    Debug function

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,18 @@
 // FUNCTION IMPLEMENTATION (MULTIPLE BUGS)
 const isPalindrome = function(str) {
-  const noSpaces = str.split(" ").join("");
+  if (str.length === 0) {
+    return false;
+  }
+
+  const noSpaces = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
   const midIndex = Math.floor(noSpaces.length/2);
   const lastIndex = noSpaces.length - 1;
 
   for (let i = 0; i < midIndex; i++) {
-    if (str[i] !== str[lastIndex - i]) return false;
+    if (noSpaces[i] !== noSpaces[lastIndex - i]) return false;
   }
-}
+  return true;
+};
 
 // Assertion Function
 const assertPalindrome = function(word, expected) {
@@ -18,7 +23,7 @@ const assertPalindrome = function(word, expected) {
   } else {
     console.log("\x1b[31m%s\x1b[0m", `\tFAIL 🛑 function returned ${actual} (expected ${expected})\n`);
   }
-}
+};
 
 
 // TEST CODE
@@ -31,5 +36,5 @@ assertPalindrome('fluff', false);
 assertPalindrome('just some random words', false);
 
 // Bonus / Stretch: Uncomment these tests and figure out why these are also failing
-// assertPalindrome('Kayak', true);
-// assertPalindrome('a santa at NASA', true);
+assertPalindrome('Kayak', true);
+assertPalindrome('a santa at NASA', true);
